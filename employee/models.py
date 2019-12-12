0ddee7512e71a0d1d1223bfda57a4746a977f4ab
bibishan-pandey <bibishan.b.pandey@gmail.com>
2019-12-12 17:25:16 +0545

    postgresql db setup and employee model migration

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class EmployeePosition(models.Model):
+    title = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    full_name = models.CharField(max_length=100)
+    emp_code = models.CharField(max_length=3)
+    mobile = models.CharField(max_length=15)
+    position = models.ForeignKey(to=EmployeePosition, on_delete=models.CASCADE)
--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'employee.apps.EmployeeConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,11 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_crud',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
